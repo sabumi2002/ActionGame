c729a243880eae5b02c71246eee2f99f59783ae7
sabeom <sabumi2002@naver.com>
2020-11-09 22:26:13 +0900

    Player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    float hAxis;
+    float vAxis;
+    bool wDown;
+
+    Vector3 moveVec;
+
+    void Start()
+    {
+        
+    }
+
+    Animator anim;
+
+    private void Awake()
+    {
+
+        anim = GetComponentInChildren<Animator>();
+    }
+    void Update()
+    {
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+        wDown = Input.GetButton("Walk");
+
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized; //어떤값이든 1로 보정
+
+        
+        transform.position += moveVec * speed * (wDown ? 0.3f : 1f) * Time.deltaTime;
+        
+
+        anim.SetBool("isRun", moveVec != Vector3.zero);
+        anim.SetBool("isWalk", wDown);
+
+        transform.LookAt(transform.position + moveVec);
+
+    }
+}
