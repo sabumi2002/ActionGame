5e04120a57100b81fec8adfe515122a4de7ceed3
sabeom <sabumi2002@naver.com>
2020-11-11 20:35:16 +0900

    swap complete

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed;
+    public GameObject[] weapons;
+    public bool[] hasWeapons;
     float hAxis;
     float vAxis;
     bool wDown;
     bool jDown;
+    bool iDown;
+    bool sDown1;    //스왑1 (무기)
+    bool sDown2;
+    bool sDown3;
 
     bool isJump;
     bool isDodge;
+    bool isSwap;
+    
 
     Vector3 moveVec;
     Vector3 dodgeVec;
@@ -19,7 +27,12 @@ public class Player : MonoBehaviour
     Rigidbody rigid;
     Animator anim;
 
-    private void Awake()
+    GameObject nearObject;  //최근에 어떤 무기오브젝트를 실행했는지 알기위해 사용
+    GameObject equipWeapon; //장착중인 웨폰은 어떤것입니까?  무기가 여러개 겹치게 들지않기위해 사용
+    int equipWeaponIndex = -1;
+
+
+    void Awake()
     {
         rigid = GetComponent<Rigidbody>();
         anim = GetComponentInChildren<Animator>();
@@ -31,14 +44,19 @@ public class Player : MonoBehaviour
         Turn();
         Jump();
         Dodge();
+        Interation();
+        Swap();
     }
     void GetInput()
     {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
         wDown = Input.GetButton("Walk");        //input manager에 버튼 생성후 사용(Walk)
-        jDown = Input.GetButtonDown("Jump");    
-
+        jDown = Input.GetButtonDown("Jump");
+        iDown = Input.GetButtonDown("Interation");
+        sDown1 = Input.GetButtonDown("Swap1");
+        sDown2 = Input.GetButtonDown("Swap2");
+        sDown3 = Input.GetButtonDown("Swap3");
     }
     void Move()
     {
@@ -46,6 +64,9 @@ public class Player : MonoBehaviour
 
         if (isDodge)
             moveVec = dodgeVec; //회피중에는 움직임벡터 -> 회피방향 벡터로 바뀌도록 구현
+
+        if (isSwap)
+            moveVec = Vector3.zero;
 
         transform.position += moveVec * speed * (wDown ? 0.3f : 1f) * Time.deltaTime;
 
@@ -61,7 +82,7 @@ public class Player : MonoBehaviour
 
     void Jump()
     {
-        if (jDown && moveVec == Vector3.zero && !isJump && !isDodge) {  //제자리에서 점프할때 점프
+        if (jDown && moveVec == Vector3.zero && !isJump && !isDodge && !isSwap) {  //제자리에서 점프할때 점프
             rigid.AddForce(Vector3.up * 15, ForceMode.Impulse);
             anim.SetBool("isJump", true);
             anim.SetTrigger("doJump");
@@ -70,7 +91,7 @@ public class Player : MonoBehaviour
     }
     void Dodge()
     {
-        if (jDown && moveVec != Vector3.zero && !isJump && !isDodge)    //움직이면서 점프할때 회피모션
+        if (jDown && moveVec != Vector3.zero && !isJump && !isDodge && !isSwap)    //움직이면서 점프할때 회피모션
         {
             dodgeVec = moveVec; //dodge 사용중일때 moveVec 복사 ( Move()에서 사용됨 )
             speed *= 2;             //스피드 2배 상승
@@ -87,6 +108,54 @@ public class Player : MonoBehaviour
         isDodge = false;
     }
 
+    void Swap()
+    {
+        if(sDown1 && (!hasWeapons[0] || equipWeaponIndex == 0))
+            return;
+        if (sDown2 && (!hasWeapons[1] || equipWeaponIndex == 1))
+            return;
+        if (sDown3 && (!hasWeapons[2] || equipWeaponIndex == 2))
+            return;
+
+        int weaponIndex = -1;
+        if (sDown1) weaponIndex = 0;
+        if (sDown2) weaponIndex = 1;
+        if (sDown3) weaponIndex = 2;
+
+        if ((sDown1 || sDown2 || sDown3) && !isJump && !isDodge) {
+            if(equipWeapon != null)
+                equipWeapon.SetActive(false);
+
+            equipWeaponIndex = weaponIndex;
+            equipWeapon = weapons[weaponIndex];
+            equipWeapon.SetActive(true);
+
+            anim.SetTrigger("doSwap");
+
+            isSwap = true;
+
+            Invoke("SwapOut", 0.4f);
+        }
+    }
+
+    void SwapOut()
+    {
+        isSwap = false;
+    }
+    void Interation()
+    {
+        if (iDown && nearObject != null && !isJump && !isDodge) {
+            if (nearObject.tag == "Weapon") {
+                Item item = nearObject.GetComponent<Item>();
+                int weaponIndex = item.value;
+                hasWeapons[weaponIndex] = true;
+
+                Destroy(nearObject);    //먹은 아이템 사라지게만듬
+            }
+        }
+    }
+
+
     void OnCollisionEnter(Collision collision)  //이 함수로 착지 구현 (충돌시)
     {
         if (collision.gameObject.tag == "Floor") {  //태그를 활용하여 바닥에 대해서만 작동하도록!
@@ -95,4 +164,17 @@ public class Player : MonoBehaviour
         }
     }
 
+
+    void OnTriggerStay(Collider other)      //d 
+    {
+        if (other.tag == "Weapon")
+            nearObject = other.gameObject;
+
+        Debug.Log(nearObject.name);
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Weapon")
+            nearObject = null;
+    }
 }
